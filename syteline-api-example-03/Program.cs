f87c94a2e1284a070ec9a2aa19b41aaa536e1c0f
Andy Mercer <61432947+amercer-fdi@users.noreply.github.com>
2025-12-01 13:20:34 -0500

    Add Example 3

---
+++ syteline-api-example-03/Program.cs
@@ -0,0 +1,64 @@
+﻿using syteline_api_examples.Helpers;
+using syteline_api_examples.Models;
+
+namespace syteline_api_examples {
+
+    internal class Program {
+
+        static void Main() {
+
+            /*********************************************************************************************/
+            /* API GUIDE - PART 3 - LOADING RECORDS - EXAMPLE 2: LOADING SIMPLE IDO, WITH PAGINATION
+            /*********************************************************************************************/
+
+            // INITIALIZE THE RESTv2 API THROUGH ION, USING THE CREDENTIALS THAT YOU DOWNLOAD AFTER CREATING AN AUTHORIZED APP AND SERVICE ACCOUNT
+            // ( FEEL FREE TO USE DIRECT CREDENTIALS HERE INSTEAD)
+
+            SytelineAPI sytelineAPI = new(
+                connection: new SytelineConnection(
+                    APIType: "ION",
+                    Config: "",
+                    CredentialsION: new(
+                        ti: "",
+                        cn: "",
+                        dt: "",
+                        ci: "",
+                        cs: "",
+                        iu: "",
+                        pu: "",
+                        oa: "",
+                        ot: "",
+                        or: "",
+                        ev: "",
+                        v: "",
+                        saak: "",
+                        sask: ""
+                    )
+                )
+            );
+
+            // LOAD A SAMPLE REQUEST
+
+            APILoadCollectionResponse response = sytelineAPI.LoadCollection(
+                idoName: "ue_FIS_CustomLoadMethodExamples",
+                properties: [
+                    "PriceCode",
+                    "Item",
+                    "ListPrice",
+                ],
+                clm: "Example_03_LoadPricesForPricecode",
+                clmParam: [
+                    "PriceCode = 'Y25'",
+                    "Item ASC"
+                ],
+                paginationMode: "propertyBased",
+                readOnly: true
+            );
+
+            Console.WriteLine($"The request retrieved {response.Items.Count} items.");
+
+        }
+
+    }
+
+}
